43f49e73ea995e5ea402ebc90120fc27f98979eb
Michael Brett <michael.brett.webdev@gmail.com>
2018-04-04 08:36:13 +0100

    Update Instagram component and styles

--- src/components/Instagram/Instagram.jsx
+++ src/components/Instagram/Instagram.jsx
@@ -2,17 +2,14 @@
 import React from 'react';
 import './Instagram.css';
 
-const Instagram = (props) => {
-  // const tags = props.tags.map((tag, index) => <p className='c-instagram__tags' key={index}>{tag}</p>);
-  return (
-    <article className="c-instagram">
-      <header className="c-instagram__header">
-        <img className="c-instagram__avatar" src="https://placehold.it/150x150" />
-        <p className="c-instagram__username">{props.user.username}</p>
-      </header>
-      <img src="https://placehold.it/320x320"/>
-      <p className="c-instagram__caption">{props.caption}</p>
-  </article>);
-};
+const Instagram = (props) =>
+  <article className="c-instagram">
+    <header className="c-instagram__header">
+      <img className="c-instagram__avatar" src="https://placehold.it/150x150" />
+      <strong className="c-instagram__username">{props.user.username}</strong>
+    </header>
+    <img src="https://placehold.it/320x320"/>
+    <p className="c-instagram__caption">{props.caption}</p>
+  </article>;
 
 export default Instagram;
