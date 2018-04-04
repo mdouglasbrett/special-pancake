cc093dc75e89efe7761ff4d46ad81029207ad2a4
Michael Brett <michael.brett.webdev@gmail.com>
2018-04-04 08:37:49 +0100

    Update Manual component

--- src/components/ManualPost/ManualPost.jsx
+++ src/components/ManualPost/ManualPost.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 import './ManualPost.css';
 
 const ManualPost = (props) =>
-  (<div className="c-manual-post">
+  <article className="c-manual-post">
     <img className="c-manual-post__image" src="https://placehold.it/320x320" />
     <p className="c-manual-post__text">{props.text}</p>
     <a className="c-manual-post__link" href={props.link} >{props['link_text']}</a>
-  </div>);
+  </article>;
 
 export default ManualPost;
